b93f9ab54dd77bd1821f1bf86283ebc817f02958
Dustin Key <Keydustin90@gmail.com>
2022-03-25 16:56:11 -0500

    Finished

---
+++ SwitchStatements/MyClass.cs
@@ -0,0 +1,35 @@
+﻿using System;
+namespace SwitchStatements
+{
+    public class MyClass
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your favorite school subject?");
+
+            var subject = Console.ReadLine();
+
+            switch (subject)
+            {
+                case "Math":
+                    Console.WriteLine("Math is Awesome!");
+                    break;
+                case "English":
+                    Console.WriteLine("English is ok.");
+                    break;
+                case "Science":
+                    Console.WriteLine("Science is also my favorite!");
+                    break;
+                case "Social Studies":
+                    Console.WriteLine("Social Studies is cool!");
+                    break;
+                case "Art":
+                    Console.WriteLine("Art is Awesome, its one of my favorite");
+                    break;
+                default:
+                    Console.WriteLine("Ah I haven't taken that subject before");
+                    break; 
+            }
+        }
+    }
+}
